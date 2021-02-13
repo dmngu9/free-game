da018901fc471e9786f5107e517088568d86aac2
Minh Nguyen <minh.nguyen@hometrack.com.au>
2021-02-13 16:54:02 +1100

    indent

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import GameList from 'components/GameList'
 import withErrorBoundary from 'hoc/withErrorBoundary'
 
